5273ead90a0ee8d1f653a26555e93027b25db447
Sampath1409 <86556890+Sampath1409@users.noreply.github.com>
2021-06-27 17:36:09 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div class="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        {"     "}
+        This camp is very useful.We can gain more information from this Javascript and
+        React.js. From this we can learn about basics and
+        how to create webpage with the help of HTML, CSS and Javascript
+        Language. Thanks to the entire team for conducting this Bootcamps.
+      </p>
+    </div>
+  );
+}
+
+export default Info;
